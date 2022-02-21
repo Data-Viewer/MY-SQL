490e895da4bf132065a0fe1ec5eecf4def20de31
Shalini J <99327803+Data-Viewer@users.noreply.github.com>
2022-02-21 19:07:25 +0530

    Add files via upload

--- Table.sql
+++ Table.sql
@@ -1,2 +1,11 @@
-select * from sakila.film
-Where (film_id between 1 and 10);
+use sakila;
+select * from actor;
+select COUNT(*) from actor;
+select COUNT(*) from address;
+select COUNT(*) from category;
+select COUNT(*) from city;
+select COUNT(*) from country;
+select COUNT(*) from customer;
+select COUNT(*) from film;
+select COUNT(*) from language;
+select COUNT(*) from staff
