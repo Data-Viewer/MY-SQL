dac0a4ff92510ec79bf1ca05963f736395a9eb51
Shalini J <99327803+Data-Viewer@users.noreply.github.com>
2022-02-21 18:29:32 +0530

    Add files via upload

---
+++ like operator.sql
@@ -0,0 +1,4 @@
+select * from sakila.city;
+select * from sakila.city where city like 'A%';
+select * from sakila.city where country_id like '8_';
+select * from sakila.city where city_id like '8_';
